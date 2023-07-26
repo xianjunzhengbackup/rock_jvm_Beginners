36feabe7ea327ac63e27677548dbd51d059a34ef
xianjunzheng <xianjunzheng.backup@gmail.com>
2023-07-26 13:28:28 +1000

    add exercise AnonymousClasses.scala

--- src/exercises/AnonymousClasses.scala
+++ src/exercises/AnonymousClasses.scala
@@ -44,6 +44,12 @@ object AnonymousClasses extends App{
     def printElements: String
 
     override def toString: String = "[" + printElements + "]"
+    def map[B](transformer: MyTransformer[A,B]):MyList[B]
+    def flatMap[B](transformer: MyTransformer[A,MyList[B]]):MyList[B]
+    def filter(predicate: MyPredicate[A]):MyList[A]
+
+    def ++[B >:A](list:MyList[B]):MyList[B]
+
   }
 
   object Empty extends MyList[Nothing] {
@@ -52,6 +58,14 @@ object AnonymousClasses extends App{
     def isEmpty: Boolean = true
     def add[B >: Nothing](element: B): MyList[B] = new Cons(element, Empty)
     override def printElements: String = ""
+
+    def map[B](transformer: MyTransformer[Nothing, B]): MyList[B]=Empty
+
+    def flatMap[B](transformer: MyTransformer[Nothing, MyList[B]]): MyList[B] =Empty
+
+    def filter(predicate: MyPredicate[Nothing]): MyList[Nothing]=Empty
+
+    def ++[B >:Nothing](list:MyList[B]):MyList[B] = list
   }
 
   //亦可写成class Cons[A](h: A, t: MyList[A]) extends MyList[A]
@@ -63,8 +77,36 @@ object AnonymousClasses extends App{
     override def printElements: String =
       if (t.isEmpty) "" + h
       else h + " " + t.printElements
+
+    def map[B](transformer: MyTransformer[A, B]): MyList[B] =
+      new Cons(transformer.transform(h),t.map(transformer))
+
+    def flatMap[B](transformer: MyTransformer[A, MyList[B]]): MyList[B] =
+      transformer.transform(h) ++ t.flatMap(transformer)
+
+    def filter(predicate: MyPredicate[A]): MyList[A] ={
+      if(predicate.test(h)) new Cons(h,t.filter(predicate))
+      else t.filter(predicate)
+    }
+
+    def ++[B >:A](list:MyList[B]):MyList[B] = new Cons(h,t ++ list)
   }
 
+  val listOfIntergers:MyList[Int] = new Cons(1,new Cons(2,new Cons(3,Empty)))
+  val listOfStrings:MyList[String] = new Cons("Hello",new Cons("Scala",Empty))
 
+  println(listOfStrings.toString)
+  println(listOfIntergers.toString)
 
+  println(listOfIntergers.map(new MyTransformer[Int,Int] {
+    override def transform(elem: Int): Int = elem * 2
+  }))
+
+  println(listOfIntergers.filter(new MyPredicate[Int] {
+    override def test(elem: Int): Boolean = elem % 2 ==0
+  }))
+
+  println(listOfIntergers.flatMap(new MyTransformer[Int,MyList[Int]] {
+    override def transform(elem: Int): MyList[Int] = new Cons(elem,new Cons(elem + 1, Empty))
+  }))
 }
