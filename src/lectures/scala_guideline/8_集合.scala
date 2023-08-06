f038c12374bdb71c4f8f2413e99cab673cc8a3c0
xianjunzhengbackup <xianjunzheng.backup@gmail.com>
2023-08-07 09:39:58 +1000

    add ...

--- src/lectures/scala_guideline/8_集合.scala
+++ src/lectures/scala_guideline/8_集合.scala
@@ -81,6 +81,7 @@ common feeds: blog.toolshed.com
 这将对每个元素应用给定的函数值、将结果收集到一个 Set 中，并最终返回这个 Set：*/
 val urls = feeds1 map ("http://" + _) 
 println(s"One url: ${urls.head}")
+println(s"One url tail: ${urls.tail}")
 /*
 我们将会看到下面的结果：
 One url: http://blog.toolshed.com 
@@ -88,33 +89,99 @@ One url: http://blog.toolshed.com
 置的迭代器 foreach()方法，如下所示：*/
 println("Refresh Feeds:") 
 feeds1 foreach { feed => println(s" Refreshing $feed...") } 
+feeds1 foreach println
 /*下面是输出结果：
 Refresh Feeds: 
  Refreshing blog.toolshed.com... 
   Refreshing pragdave.me... 
    Refreshing blog.agiledeveloper.com... 
-   这就是元素的无序集合。接下来，让我们探讨关联映射—Map。
-   8.3 关联映射 
-   假设我们要将 feed 的作者的名字附加到 feed 上，我们可以将其以键值对的形式存储在
-   Map 中。
-   val feeds = Map( 
-      "Andy Hunt" -> "blog.toolshed.com", 
-       "Dave Thomas" -> "pragdave.me", 
-        "NFJS" -> "nofluffjuststuff.com/blog") 
-   如果想要得到一个 feed 的 Map，其中 feed 的作者名开头都为“D”，那么我们可以使用
-   filterKeys()方法。
-   val filterNameStartWithD = feeds filterKeys (_ startsWith "D") 
-   println(s"# of Filtered: ${filterNameStartWithD.size}") 
-   下面是输出结果：
+   这就是元素的无序集合。接下来，让我们探讨关联映射—Map。*/
+println("s--------8.3 关联映射----------") 
+/*   假设我们要将 feed 的作者的名字附加到 feed 上，我们可以将其以键值对的形式存储在
+   Map 中。*/
+val feeds = Map( 
+  "Andy Hunt" -> "blog.toolshed.com", 
+  "Dave Thomas" -> "pragdave.me", 
+  "NFJS" -> "nofluffjuststuff.com/blog") 
+/*   如果想要得到一个 feed 的 Map，其中 feed 的作者名开头都为“D”，那么我们可以使用
+   filterKeys()方法。*/
+val filterNameStartWithD = feeds filterKeys (_ startsWith "D") 
+println(s"# of Filtered: ${filterNameStartWithD.size}") 
+/*   下面是输出结果：
    # of Filtered: 1 
    另外，如果想要对这些值进行筛选，那么除对键进行操作之外，我们还可以使用 filter()
    方法。提供给 filter()方法的函数值接收一个（键，值）元组，我们可以像下面这样使
-   用它：
-   val filterNameStartWithDAndPragprogInFeed = feeds filter { element => 
-      val (key, value) = element 
-       (key startsWith "D") && (value contains "pragdave") 
-   } 
-   print("# of feeds with auth name D* and pragdave in URL: ") 
-   println(filterNameStartWithDAndPragprogInFeed.size)
+   用它：*/
+val filterNameStartWithDAndPragprogInFeed = feeds filter { element => 
+  val (key, value) = element 
+  (key startsWith "D") && (value contains "pragdave") 
+} 
+print("# of feeds with auth name D* and pragdave in URL: ") 
+println(filterNameStartWithDAndPragprogInFeed.size)
+/*
+ 下面是输出结果：
+ # of feeds with auth name D* and pragdave in URL: 1 
+ 如果需要获取一个人的 feed，只需要使用 get()方法。因为对于给定的键，其对应的值
+ 可能不存在，所以 get()方法的返回类型是 Option[T]（参见 5.2.3 节）。该方法的实际结
+ 果要么是一个 Some[T]要么是 None，其中 T 也是 Map 的值类型。*/
+ println(s"Get Andy's Feed: ${feeds.get("Andy Hunt")}") 
+ println(s"Get Bill's Feed: ${feeds.get("Bill Who")}") 
+/*上述代码的输出结果如下：
+ Get Andy's Feed: Some(blog.toolshed.com) 
+ Get Bill's Feed: None 
+ 此外，我们可以使用 apply()方法来获取一个键的值。需要牢记的是，这是我们在类或
+ 者实例后加上圆括号时，Scala 调用的方法。但是，和 get()方法不同的是，apply()方法
+ 返回的不是 Option[T]，而是返回的值（即 T）。小心使用—确保将代码放置在一个
+ try-catch 代码块中。①*/
+ try { 
+    println(s"Get Andy's Feed Using apply(): ${feeds("Andy Hunt")}") 
+     print("Get Bill's Feed: ") 
+      println(feeds("Bill Who")) 
+ } catch { 
+    case _: java.util.NoSuchElementException => println("Not found") 
+ } 
+/* 下面是使用 apply()方法的输出结果：
+ Get Andy's Feed Using apply(): blog.toolshed.com 
+ Get Bill's Feed: Not found 
+ 要添加 feed，请使用 updated()方法。因为我们使用的是不可变集合，所以 updated()
+ 方法不会影响原来的 Map。如同其方法名所提示的一样，它会返回一个携带着新元素的全新
+ Map。*/
+ val newFeeds1 = feeds.updated("Venkat Subramaniam", "blog.agiledeveloper.com") 
+ println("Venkat's blog in original feeds: " + feeds.get("Venkat Subramaniam")) 
+ println("Venkat's blog in new feed: " + newFeeds1("Venkat Subramaniam")) 
+/* 让我们看一下调用 updated()方法之后的效果：
+ Venkat's blog in original feeds: None 
+ Venkat's blog in new feed: blog.agiledeveloper.com 
+ 除显式地调用 updated()方法之外，也可以利用另一个 Scala 小技巧。如果在赋值语句
+ 的左边的类或者实例上使用圆括号，那么 Scala 将自动调用 updated()方法。因此，X() = 
+   b 等价于 X.updated(b)。如果 updated()接受多个参数，那么可以将除尾参数之外的所
+   有参数都放置在括号内部。因此，X(a) = b 等价于 X.updated(a,b)。
+   我们可以在不可变集合上使用该隐式调用，像这样：val newFeed = feeds("author") 
+   = "blog"。但是，多重赋值语句这种形式，使其失去了语法上的优雅性，后一个等号用于
+   调用 updated()方法，而前一个用于保存新创建的 Map。如果我们要从一个方法中返回新
+   创建的 Map，那么隐式的 updated()方法使用起来就很优雅。然而，如果我们想要就地更
+   新 Map，那么在可变集合上使用该隐式调用则更加具有意义。
+   val mutableFeeds = scala.collection.mutable.Map( 
+      "Scala Book Forum" -> "forums.pragprog.com/forums/87") 
+   mutableFeeds("Groovy Book Forum") = "forums.pragprog.com/forums/246" 
+   println(s"Number of forums: ${mutableFeeds.size}") 
+   我们将得到如下输出结果：
+   Number of forums: 2 
+   既然已经学习了 Set 和 Map，那么我们就不能再忽略最常见的集合—List。
+   8.4 不可变列表 
+   通过使用 head 方法，Scala 使访问一个列表的第一个元素更加简单快速。使用 tail 方
+   法，可以访问除第一个元素之外的所有元素。访问列表中的最后一个元素需要对列表进行遍
+   历，因此相比访问列表的头部和尾部①，该操作更加昂贵。所以，列表上的大多数操作都是
+   围绕着对头部和尾部的操作构造的。
+   让我们继续使用上面的 feed 例子来学习 List。我们可以使用 List 来维护一个有序的
+   feed 集合。
+   val feeds = List("blog.toolshed.com", "pragdave.me", "blog.agiledeveloper.com") 
+   这创建了一个 List[String]的实例。我们可以使用从 0 到 list.length - 1 的索
+   引来访问 List 中的元素。当调用 feeds(1)方法时，我们使用的是 List 的 apply()方法。
+   也就是说，feeds(0)是 feeds.apply(0)的一个简单形式。要访问第一个元素，我们可以
+   使用 feeds(0)或者 head()方法。
+   println(s"First feed: ${feeds.head}") 
+   println(s"Second feed: ${feeds(1)}") 
+   这段代码的输出结果如下：
 */
 }
